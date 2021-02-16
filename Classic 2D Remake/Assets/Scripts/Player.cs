becd23431afa9a7e331581c96dcfa85f03f3031b
Missingwings <67066971+Missingwings@users.noreply.github.com>
2021-02-16 16:44:29 -0500

    Added script and movement to player character

---
+++ Classic 2D Remake/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        //Controls
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 position = transform.position;
+        position.x = position.x + 5.0f * horizontal * Time.deltaTime;
+        transform.position = position;
+    }
+
+}
