5d3cf8957ffc5758b7c891b591cfad04312be231
Missingwings <67066971+Missingwings@users.noreply.github.com>
2021-02-16 17:21:21 -0500

    Worked on movement/jump in script, debugging?

--- Classic 2D Remake/Assets/Scripts/Player.cs
+++ Classic 2D Remake/Assets/Scripts/Player.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float speed = 5.0f;
+    public float jump = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +26,16 @@ public class Player : MonoBehaviour
         Vector2 position = transform.position;
         position.x = position.x + 5.0f * horizontal * Time.deltaTime;
         transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce (Vector2.up * jump);
     }
 
 }
