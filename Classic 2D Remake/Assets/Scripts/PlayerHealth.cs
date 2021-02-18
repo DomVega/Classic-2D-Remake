d8519d54a8167fb677b7fa2e2f44de7c634dbf3c
Missingwings <67066971+Missingwings@users.noreply.github.com>
2021-02-17 19:11:20 -0500

    Added basic death script

---
+++ Classic 2D Remake/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public bool autoDeath;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        autoDeath = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.y < -5)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene("Level-1");
+    }
+}
